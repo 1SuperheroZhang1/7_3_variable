ae62274a5324c84008e6949ea7b5d8c13b165532
zhangcy <2304417358@qq.com>
2023-01-22 12:57:31 +0800

    C语言督学营 - 7.3 全局变量和局部变量

---
+++ cmake-build-debug/CMakeFiles/7_3_variable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7_3_variable.exe"
+  "7_3_variable.exe.manifest"
+  "7_3_variable.pdb"
+  "CMakeFiles/7_3_variable.dir/main.c.obj"
+  "lib7_3_variable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/7_3_variable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
